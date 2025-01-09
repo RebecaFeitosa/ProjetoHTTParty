ed1783751e1642b5b13b158010a16cdeba124db2
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2025-01-08 22:46:06 -0300

    criando modulo para url base e para os headers

---
+++ features/support/base_url.rb
@@ -0,0 +1,7 @@
+# Melhorando a qualidade do código, módulo com a url base da aplicação e os headers que serão usados em cada chamada
+module User
+    include HTTParty
+    base_uri 'https://reqres.in/api/users'
+    format :json
+    headers 'Content-Type': 'application/json'
+end
