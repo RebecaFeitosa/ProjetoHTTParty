359defce033d2e85cb5df0a721bf8adc63550cd0
Rebeca Feitosa <rebeca.feitosa60@gmail.com>
2025-01-10 12:08:04 -0300

    criando arquivo para passar as informacoes dos usuarios

---
+++ features/support/database.rb
@@ -0,0 +1,22 @@
+# coding: utf-8
+
+DATABASE = {
+        name: {
+            name1: 'Luke',
+            name2: 'Luciano',
+            name3: 'Alberto',
+            name4: 'Vanda'
+        },
+
+        age: {
+            age1: 30,
+            age2: 28,
+            age3: 23,
+            age4: 25
+        },
+
+        gender: {
+            gender1: 'male',
+            gender2: 'female'
+        }
+}
